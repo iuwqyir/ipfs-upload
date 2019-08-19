d8be0a52a4958acc0d706a1fd2c09c4ac8656bb7
Iuwqyir <toomas.oosalu@gmail.com>
2019-08-19 22:02:10 +0100

    ADD ipfs configuration

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import SimpleStorageContract from "./contracts/SimpleStorage.json";
 import getWeb3 from "./utils/getWeb3";
+import ipfs from './ipfs';
 
 import "./App.css";
 
